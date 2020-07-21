8f4c649c48363e59dcaad98a4e1c88acdf9a195d
Matthew Siegfried <siegfried@mines.edu>
2020-07-21 02:01:12 -0600

    fix cv typo

---
+++ update_website.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "need to include a message"
+    exit 0
+fi
+
+hugo
+git add *
+git commit -a -m"$1"
+git push
+cd public
+git add *
+git commit -a -m$1
+git push
+cd ..
+echo "done updating website"
